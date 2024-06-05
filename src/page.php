05a5844d89ed631725c9dcbf5a1ec7ba1f69ab75
Iurii Stepanenko <iuriis.develop@gmail.com>
2024-06-05 10:28:32 +0300

    Implement basic routing. Added basic pages, data.php file for categories and products.

---
+++ src/page.php
@@ -0,0 +1,65 @@
+<?php
+
+require_once '../src/data.php';
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Travel Blog</title>
+    <style>
+        header,
+        main,
+        footer {
+            border: 1px dashed black;
+        }
+
+        .post-list {
+            display: flex;
+        }
+
+        .post-list .post {
+            max-width: 30%;
+        }
+    </style>
+</head>
+<body>
+<header>
+    <a href="/" title="Travel Blog">
+        <img src="/logo.png" alt="Logo" width="200"/>
+    </a>
+    <nav>
+        <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/blog">Blog</a></li>
+            <ul><span>Categories</span>
+                <?php foreach (catalogGetCategory() as $category) : ?>
+                    <li>
+                        <a href="/<?= $category['url'] ?>"><?= $category['name'] ?></a>
+                    </li>
+                <?php endforeach; ?>
+            </ul>
+        </ul>
+    </nav>
+</header>
+
+<main>
+    <?php require_once "../src/pages/$page" ?>
+</main>
+
+<footer>
+    <nav>
+        <ul>
+            <li>
+                <a href="/about">About Us</a>
+            </li>
+            <li>
+                <a href="/contact">Contact Us</a>
+            </li>
+        </ul>
+    </nav>
+    <p>© Iurii Stepanenko <?= date("Y"); ?>. All Rights Reserved.</p>
+</footer>
+</body>
+</html>
